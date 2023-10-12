eebe307a69183840ab35f9bc77de4890439037b6
VincentProuchet <prouchetvincent@hotmail.com>
2023-10-12 11:47:45 +0200

    mise en place de l'api consumer
    api-consumer.service
     getfestivals

--- src/api-consumer/api-consumer.service.ts
+++ src/api-consumer/api-consumer.service.ts
@@ -1,11 +1,13 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { join } from 'path';
+import { FestivalEntity } from 'src/festival/entities/festival.entity';
 
 @Injectable()
 export class ApiConsumerService {
   private baseUrl: string = process.env.EXTERNAL_API_BASE_URL ?? "";
-  private responseType: string = "response_type=json"
+  private responseType: string = "response_type=json";
+  private fetch_size = 100;
 
   constructor(private readonly httpService: HttpService) {
   }
@@ -13,5 +15,15 @@ export class ApiConsumerService {
   async getUsers(usersNumber: number): Promise<any> {
     const response = await this.httpService.axiosRef.get(join(this.baseUrl, "users", `?${this.responseType}&number=${usersNumber}`));
     return response.data;
+  }
+  /**
+   * retourne un json de festival
+   * ils ne les retourne que par un maximul de 100 à chaque fois
+   * le paramètre offset permet de définir le point de départ de la liste 
+   * @param offset point de départ de la liste 
+   * @returns 
+   */
+  async getFestivals(offset: number = 0):Promise<any>{
+     return (await this.httpService.axiosRef.get(`this.baseUrl?limit=${this.fetch_size}&offset=${offset}`)).data;
   }
 }
