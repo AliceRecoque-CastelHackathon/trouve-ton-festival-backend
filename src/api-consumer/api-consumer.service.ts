ac22c8cb35ae4f9a6e9054530d4a95acdbed1f91
VincentProuchet <prouchetvincent@hotmail.com>
2023-10-12 13:33:37 +0200

    création boucle de récupération des féstivals
    cration interfaces

--- src/api-consumer/api-consumer.service.ts
+++ src/api-consumer/api-consumer.service.ts
@@ -1,15 +1,19 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 import { join } from 'path';
 import { FestivalEntity } from 'src/festival/entities/festival.entity';
+import { i_open_data_festival_response } from './interface/i_open_data_festival_response';
+import { throws } from 'assert';
+import { json } from 'stream/consumers';
+import { FestivalService } from 'src/festival/festival.service';
 
 @Injectable()
 export class ApiConsumerService {
   private baseUrl: string = process.env.EXTERNAL_API_BASE_URL ?? "";
   private responseType: string = "response_type=json";
-  private fetch_size = 100;
+  private fetch_size:number = 100;
 
-  constructor(private readonly httpService: HttpService) {
+  constructor(private readonly httpService: HttpService, private readonly festivalService: FestivalService) {
   }
 
   async getUsers(usersNumber: number): Promise<any> {
@@ -24,22 +28,21 @@ export class ApiConsumerService {
    * @returns 
    */
   async getFestivals(offset: number = 0){
-    const festivals_counts = 0;
+    let festivals_counts:number = 0;
     
     do{
       try {
         const response = await this.httpService.axiosRef.get(`this.baseUrl?limit=${this.fetch_size}&offset=${offset}`);
-        
-      } catch (error) {
-        
-      }
-      if (festivals_counts== 0){
-        try {
-          //festivals_counts = response.data.
-        } catch (error) {
-          
+        const responseObject: any = JSON.parse(response.data);
+        if (festivals_counts== 0){
+          festivals_counts = responseObject["total_count"];
         }
+        //@TODO appel de festival service pour lui passer la liste à enregistrer
+
+      } catch (error) {
+        throw new BadRequestException("le serveur de l'api externe n'as pas donnée de réponse");
       }
+
 
     }while (offset < festivals_counts- this.fetch_size);
 
---
+++ src/api-consumer/interface/i_open_data_festival_response.ts
@@ -0,0 +1,6 @@
+import { i_open_data_festival } from "./i_open_data_festival";
+
+export interface i_open_data_festival_response{
+    total_count : number,
+    results : i_open_data_festival[]
+}
