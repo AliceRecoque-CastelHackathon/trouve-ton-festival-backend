2b37a757f5e33a48a9c2802c50b415681ecd2079
Erwan MORIO <erwan.morio@jasparo.fr>
2023-10-12 12:36:35 +0200

    edit getAll controller to get queryParams limit and offset

--- src/festival/festival.controller.ts
+++ src/festival/festival.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import { Controller, Get, Req } from '@nestjs/common';
+import { ApiBearerAuth, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+
 import { FestivalGetDto } from './dto/out/festival-get.dto';
 import { FestivalService } from './festival.service';
 
+@ApiBearerAuth()
+@ApiTags('festival')
 @Controller('festival')
 export class FestivalController {
   constructor(private readonly festivalService: FestivalService) {}
 
+  @ApiQuery({
+    name: 'limit',
+    style: 'simple',
+    required: false
+  })
+  @ApiQuery({
+    name: 'offset',
+    style: 'simple',
+    required: false
+  })
   @ApiResponse({
-    description: 'A list of n festival',
+    description: 'A list of festival',
     type: [FestivalGetDto],
   })
   @Get('all')
-  async findAll(limit: number, startId: number): Promise<FestivalGetDto[]> {
-    return [new FestivalGetDto()];
+  async findAll(@Req() req: Request): Promise<FestivalGetDto[]> {
+    const limit: number = req.query.limit ? parseInt(req.query.limit.toString()) : 10
+    const offset: number = req.query.offset ? parseInt(req.query.offset.toString()) : 0
+    return this.festivalService.getAll(limit, offset);
   }
 }
--- src/festival/festival.module.ts
+++ src/festival/festival.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FestivalController } from './festival.controller';
 import { FestivalService } from './festival.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FestivalEntity } from './entities/festival.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FestivalEntity])],
   controllers: [FestivalController],
   providers: [FestivalService]
 })
