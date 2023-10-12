1393ad88319280c6edcd52e501c76bdc91f0e13b
Erwan MORIO <erwan.morio@jasparo.fr>
2023-10-12 12:02:11 +0200

    add getAll endpoint and service method

--- src/festival/festival.controller.ts
+++ src/festival/festival.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
+import { FestivalGetDto } from './dto/out/festival-get.dto';
+import { FestivalService } from './festival.service';
 
 @Controller('festival')
-export class FestivalController {}
+export class FestivalController {
+  constructor(private readonly festivalService: FestivalService) {}
+
+  @ApiResponse({
+    description: 'A list of n festival',
+    type: [FestivalGetDto],
+  })
+  @Get('all')
+  async findAll(limit: number, startId: number): Promise<FestivalGetDto[]> {
+    return [new FestivalGetDto()];
+  }
+}
--- src/festival/festival.service.ts
+++ src/festival/festival.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FestivalEntity } from './entities/festival.entity';
+import { Repository } from 'typeorm';
+import { FestivalGetDto } from './dto/out/festival-get.dto';
 
 @Injectable()
-export class FestivalService {}
+export class FestivalService {
+  constructor(
+    @InjectRepository(FestivalEntity)
+    private festivalsRepository: Repository<FestivalEntity>,
+  ) { }
+
+  async getAll(limit: number, offset: number): Promise<FestivalGetDto[]> {
+    const entities: FestivalEntity[] = await this.festivalsRepository.find({
+      skip: offset,
+      take: limit
+    });
+    const results: FestivalGetDto[] = [];
+
+    entities.forEach(async (festivalEntity: FestivalEntity) => {
+      results.push(new FestivalGetDto(festivalEntity));
+    });
+
+    return results;
+  }
+
+}
